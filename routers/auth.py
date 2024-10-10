cede530120d2c37e9168fd2186e9f0cdc2db7453
Gabrielle Mitie <99052048+gabriellemitie@users.noreply.github.com>
2024-10-10 10:43:58 -0300

    Update auth.py

--- routers/auth.py
+++ routers/auth.py
@@ -74,7 +74,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Não foi possível validar o usuário.')
 
-# criandeo usuario
+# criando usuario
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
